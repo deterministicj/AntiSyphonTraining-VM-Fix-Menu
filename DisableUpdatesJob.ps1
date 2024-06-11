337595a4dde6a88485d84ad7788ae6fc1eb4d330
deterministicj <77554249+deterministicj@users.noreply.github.com>
2024-06-11 14:02:30 -0600

    Add files via upload

---
+++ DisableUpdatesJob.ps1
@@ -0,0 +1,6 @@
+﻿$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy Bypass -File "c:\DisableUpdates.ps1"'
+
+$trigger = New-ScheduledTaskTrigger -AtLogon
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Disable Updates" -RunLevel Highest
+Start-ScheduledTask -TaskName "Disable Updates"
