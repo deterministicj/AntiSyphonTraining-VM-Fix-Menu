610d708b4281d698590a1f47a5b03c29dde7319c
deterministicj <77554249+deterministicj@users.noreply.github.com>
2024-06-12 12:57:12 -0600

    Add fix menu

--- DisableUpdatesJob.ps1
+++
@@ -1,7 +0,0 @@
-﻿$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy Bypass -File "c:\DisableUpdates.ps1"'
-
-$trigger = New-ScheduledTaskTrigger -AtLogon
-
-Invoke-webRequest -Uri https://github.com/deterministicj/AntiSyphonTraining-VM-disable-updates/blob/main/scripts/DisableUpdates.ps1?raw=true -OutFile C:\DisableUpdates.ps1
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Disable Updates" -RunLevel Highest
-Start-ScheduledTask -TaskName "Disable Updates"
