fa98de4b82985aec6d6153665ab930308274ee37
deterministicj <77554249+deterministicj@users.noreply.github.com>
2024-06-12 14:17:12 -0600

    Change Github link to reflect repo name change

--- scripts/DisableUpdatesJob.ps1
+++ scripts/DisableUpdatesJob.ps1
@@ -2,6 +2,6 @@
 
 $trigger = New-ScheduledTaskTrigger -AtLogon
 
-Invoke-webRequest -Uri https://github.com/deterministicj/AntiSyphonTraining-VM-disable-updates/blob/main/scripts/DisableUpdates.ps1?raw=true -OutFile C:\DisableUpdates.ps1
+Invoke-webRequest -Uri https://github.com/deterministicj/AntiSyphonTraining-VM-Fix-Menu/blob/main/scripts/DisableUpdates.ps1?raw=true -OutFile C:\DisableUpdates.ps1
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Disable Updates" -RunLevel Highest
 Start-ScheduledTask -TaskName "Disable Updates"
