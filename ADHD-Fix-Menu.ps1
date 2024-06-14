a3e6302bac34929549f4cc9c076adf345c98b2e3
deterministicj <77554249+deterministicj@users.noreply.github.com>
2024-06-14 14:15:28 -0600

    Beautify and error handling

--- ADHD-Fix-Menu.ps1
+++ ADHD-Fix-Menu.ps1
@@ -1,49 +1,87 @@
-#Main Menu w/ input
-Write-Output "ADHD Lab Fix Menu"
-Write-Output "`n---------------------------------------------------"
-Write-Output "1. Disable automatic updates"
-Write-Output "2. Fix Hashcat lab - CL_DEVICE_NOT_AVAILABLE error"
-Write-Output "---------------------------------------------------`n"
+
+$banner = @"
+    _    ____  _   _ ____    _____ _        __  __                  
+   / \  |  _ \| | | |  _ \  |  ___(_)_  __ |  \/  | ___ _ __  _   _ 
+  / _ \ | | | | |_| | | | | | |_  | \ \/ / | |\/| |/ _ \ '_ \| | | |
+ / ___ \| |_| |  _  | |_| | |  _| | |>  <  | |  | |  __/ | | | |_| |
+/_/   \_\____/|_| |_|____/  |_|   |_/_/\_\ |_|  |_|\___|_| |_|\__,_|                                                                                         
+"@
+
+$menu = @"
+Options
+---------------------------------------------------
+1. Disable automatic updates
+2. Fix Hashcat lab - CL_DEVICE_NOT_AVAILABLE error
+---------------------------------------------------
+"@
+
+Write-Host $banner
+Write-Host " `n"
+Write-Host $menu -ForegroundColor Cyan
+Write-Host "`n"
 
 Function DisableUpdates {
     #Create Disable Updates task if it does not exist
-    Write-Output "Checking if task already exists..."
+    Write-Host "Checking if task already exists..."
     $taskExists = Get-ScheduledTask -TaskName "Disable Updates" -ErrorAction SilentlyContinue
 
     if (-not $taskExists) {
-        Write-Output "Downloading required scripts"
+        Write-Host "Downloading required scripts"
         Invoke-webRequest -Uri https://github.com/deterministicj/AntiSyphonTraining-VM-disable-updates/blob/main/scripts/DisableUpdatesJob.ps1?raw=true -OutFile C:\DisableUpdatesJob.ps1
         C:\DisableUpdatesJob.ps1 >$null
-        Write-Output "Task Created - Automatic updates have been disabled"
+        
+        try { 
+            Get-ScheduledTask -TaskName "Disable Updates" -ErrorAction Stop
+            Write-Host "Task Created - Automatic updates have been disabled"
+        }
+        catch {
+            Write-Host "Error:$_"
+            Write-Host "There was an issue creating the task, please try again"
+        }
     } else {
-        Write-Output "Task already exists"
+        Write-Host "Task already exists"
     }
 }
 
 Function HashcatFix {
     #Download and install Intel OpenCL update
-    Write-Output "Downloading Intel OpenCL Update..."
+    Write-Host "Downloading Intel OpenCL Update..."
     Invoke-WebRequest -Uri http://registrationcenter-download.intel.com/akdlm/irc_nas/12512/opencl_runtime_16.1.2_x64_setup.msi -OutFile C:\Users\ADHD\Downloads\opencl_runtime_16.1.2_x64_setup.msi
     msiexec /i C:\Users\adhd\Downloads\opencl_runtime_16.1.2_x64_setup.msi /quiet
-    Write-Output "Update installed, please try running Hashcat again"
+    
+    try {
+        $regPath = 'HKLM:\SOFTWARE\Intel\OpenCL'
+        $regKey = 'cpu_version'
+        $openCLVersion = Get-ItemPropertyValue -Path $regPath -Name $regKey -ErrorAction SilentlyContinue
+
+        if ($openCLVersion -eq "6.4.0.37") {
+            Write-Host "Update installed, please try running Hashcat again"
+        } else {
+            Write-Host "Update failed, please try installing again"
+        }    
+
+    } catch {
+        Write-Host "There was an issue checking the installed version of Intel OpenCL, please try installing again"
+        Write-Host "Error: $_"
+    }
 }
 
 # Loop for input
 do {
-    $input = Read-Host -Prompt "Which option would you like to select? (1,2, or q to exit)"
+    $userInput = Read-Host -Prompt "Which option would you like to select? (1,2, or q to exit)"
     
     # Check for valid input
-    if ($input -eq "1") {
+    if ($userInput -eq "1") {
         DisableUpdates
         break
-    } elseif ($input -eq "2") {
+    } elseif ($userInput -eq "2") {
         HashcatFix
         break
-    } elseif ($input -eq "q") {
+    } elseif ($userInput -eq "q") {
         # Exit loop if 'q' is entered
         break
     } else {
         # Handle invalid input
-        Write-Output "Invalid option. Please select a valid option (1, 2, or q to exit)."
+        Write-Host "Invalid option. Please select a valid option."
     }
 } until ($false)
